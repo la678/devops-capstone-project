dfbb44d7c8d020c19f3db5d1195200cdd63e1353
la678 <lanchuri@ttu.edu>
2024-03-12 19:34:18 -0400

    corrected lint errors in routes.py

--- service/routes.py
+++ service/routes.py
@@ -64,11 +64,11 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 
 
-@app.route("/accounts", methods = ["GET"])
+@app.route("/accounts", methods=["GET"])
 def list_all_accounts():
     app.logger.info("Request to list all accounts")
-    accounts= Account.all()
-    account_list = [account.serialize() for account in accounts]    
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -79,7 +79,7 @@ def list_all_accounts():
 # ... place you code here to READ an account ...
 
 
-@app.route("/accounts/<int:account_id>", methods = ["GET"])
+@app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
@@ -94,7 +94,7 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 
 
-@app.route("/accounts/<int:account_id>", methods = ["POST", "PUT"])
+@app.route("/accounts/<int:account_id>", methods=["POST", "PUT"])
 def update_accounts(account_id):
     """
     Update an Account
@@ -116,7 +116,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-    
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
